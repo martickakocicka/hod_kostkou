2bfb24e8fe2baaa4e0644995656b76140e888499
Marta Bakešová <marta.bakesova@centrum.cz>
2024-02-04 13:33:49 +0100

    Opravil jsem zobrazovani

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
 let cislo = Math.floor(Math.random() * 6) + 1;
 console.log ("na kostce padlo cislo: " + cislo);
 
-document.body.innerHTML= "na kostce padlo číslo: " + cislo
+document.body.innerHTML += "<p> na kostce padlo číslo: " + cislo + "</p>" // Tady pouzit += misto = a pridat <p>, aby se to zobrazilo pod nadpisem v novem odstavci.

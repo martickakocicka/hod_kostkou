57afbbffa172e980c7dd2faca63b800be3462ec2
Marta Bakešová <marta.bakesova@centrum.cz>
2024-02-20 23:00:19 +0100

    doplneni komentare

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ukol31_1
+Math.floor a Math.random, omezeni math random
--- script2.js
+++ script2.js
@@ -1,4 +1,12 @@
+//Math.random()vrátí náhodné číslo mezi 0 (včetně) a 1 (mimo)
+//Math.random()použitý s Math.floor()lze použít k vrácení náhodných celých čísel.
+//Příklad: Returns a random integer from 0 to 10:
+//         Math.floor(Math.random() * 11)
+//Příklad: Returns a random integer from 1 to 10:
+//         Math.floor(Math.random() * 10) + 1
+
 let cislo = Math.floor(Math.random() * 6) + 1;
 console.log ("na kostce padlo cislo: " + cislo);
 
-document.body.innerHTML += "<p> na kostce padlo číslo: " + cislo + "</p>" // Tady pouzit += misto = a pridat <p>, aby se to zobrazilo pod nadpisem v novem odstavci.
+document.body.innerHTML += "<p> na kostce padlo číslo: " + cislo + "</p>" 
+// Tady pouzit += misto = a pridat <p>, aby se to zobrazilo pod nadpisem v novem odstavci.
